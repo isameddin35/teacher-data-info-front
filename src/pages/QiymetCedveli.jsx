bdabc9b26f7ebc588e4332093c34082e053927fe
Isameddin <isaqurbanov112@gmail.com>
2025-08-20 12:56:51 +0400

    Link imported

--- src/pages/QiymetCedveli.jsx
+++ src/pages/QiymetCedveli.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react"; 
+import { Link } from "react-router-dom";
+
 
 export default function QiymetCedveli() {
   const BASE_URL = "https://teacher-data-system-api.onrender.com"
