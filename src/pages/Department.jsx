db3c48099b962511f09d48796d7ad3a879f52c68
Isameddin <isaqurbanov112@gmail.com>
2025-09-22 22:37:21 +0400

    last one

--- src/pages/Department.jsx
+++ src/pages/Department.jsx
@@ -138,7 +138,7 @@ export default function QiymetCedveli() {
               className="btn btn-primary w-100"
               onClick={() => {
                 localStorage.clear();
-                window.location.href = BASE_URL;
+                window.location.href = `${BASE_URL}/auth`;
               }}
             >
               Çıxış et
